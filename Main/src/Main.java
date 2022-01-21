e29d3de1389fa5e2acdc248d959db079055bb36f
Apulsar <themrmike98@hotmail.es>
2022-01-21 10:40:41 +0100

    cuarto commit

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ey, estamos viendo si esto funca o no");
+		
+	}
+	public void jajaSi() {
+		System.out.println("Holiwi");
 	}
 
 }
